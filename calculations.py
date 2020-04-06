785ff318200447ab4c0163d07e609d689ad2b45a
lwatsky <60194040+lwatsky@users.noreply.github.com>
2020-04-06 14:30:04 -0400

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,7 @@
+#Final Project
+#Lauren Watsky, Katie Wolberg, Jamie Zuckerman
+
+#this file will select data from the database and calculate values from it
+#it will output the calculated data to a file
+#can store the calculated data in the database
+#can write out the visualizations in this program or another from the stores calculations
