b667c9543c644940cb50f0026aa1d9afad20a5cb
lwatsky <60194040+lwatsky@users.noreply.github.com>
2020-04-06 14:27:44 -0400

    Add files via upload

---
+++ read-data.py
@@ -0,0 +1,9 @@
+#Final Project
+#Lauren Watsky, Katie Wolberg, Jamie Zuckerman
+
+#this file will be used to read the data from the APIs and stores the data in the database
+
+#must limit to 20 items at a time added to the database for the first 100 rows stored
+
+
+
